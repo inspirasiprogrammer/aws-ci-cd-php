14ab8d9048e76b38d352bdb0a0ded7c2bba2174f
ACER\irwan <irwansyahputra92@gmail.com>
2022-01-03 15:16:42 +0700

    Push

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
 <h1>This is a PHP Deploy CI CD</h1>
 <p>This is a simple testing deploy to aws.</p>
 <p>This Tutorial has Created at Irwan Syahputra</p>
-<a href="https://anakmedan.id">Berita Anak Medan</a>
-<a href="https://inspirasiprogrammer.id">Tutorial IT</a>
-<a href="https://bolahita.id">Berita Seputar Bola</a>
+<a href="https://anakmedan.id">Berita Anak Medan</a></br>
+<a href="https://inspirasiprogrammer.id">Tutorial IT</a></br>
+<a href="https://bolahita.id">Berita Seputar Bola</a></br>
 </body>
 </html>
